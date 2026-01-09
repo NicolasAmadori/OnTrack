8f9c9b58410b6465804173d98aed40538fbaa1b4
Nicolas Amadori <nicolas.amadori02@gmail.com>
2026-01-09 16:33:44 +0100

    feat: added get_stations endpoint in frontend

---
+++ nodejs/frontend/src/api/stations.js
@@ -0,0 +1,21 @@
+import { API_BASE_URL } from '@/utils/constants.js';
+import { checkResponseSuccess } from "./util";
+
+export async function get_stations(authToken, name) {
+    var endpoint = `${API_BASE_URL}/stations/search`;
+    if (name) {
+        endpoint += `?station_name=${name}`
+    }
+
+    const response = await fetch(endpoint, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${authToken}`
+        },
+    });
+
+    const data = await response.json();
+    checkResponseSuccess(response, data);
+    return data.stations;
+}
