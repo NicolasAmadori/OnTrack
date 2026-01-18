22e180c90b08df3b76ea92f83f808ef7b35ef8cf
Riccardo Mazzi <riccardo.mazzi@studio.unibo.it>
2026-01-18 16:56:35 +0100

    feat: finished reservationManagement

--- nodejs/frontend/src/views/admin/ReservationManagement.vue
+++ nodejs/frontend/src/views/admin/ReservationManagement.vue
@@ -1,52 +1,84 @@
 <template>
   <BaseBanner title="Reservation Management" imageType="tickets" :subtitle="displaySubtitle" admin goBackIcon/>
-
-  <div class="container-fluid mt-4">
-    <div class="row mt-4 justify-content-center">
-      <div class="col-12 col-md-8">
-
-        <div v-if="isLoading" class="text-center py-5">
-          <span class="spinner-border text-primary" role="status"></span>
+    <div class="max-w-4xl mx-auto p-4">
+      <div v-if="reservations.length === 0" class="text-center text-disabled mt-10">
+        User has no reservations yet.
+      </div>
+      <div v-else class="space-y-6">
+        <div 
+          v-for="reservation in reservations" 
+          :key="reservation._id"
+        >
+          <ReservationCard
+            :departure_time="reservation.departure_time"
+            :arrival_time="reservation.arrival_time"
+            :origin="reservation.origin"
+            :destination="reservation.destination"
+            :duration="reservation.duration"
+            :price_amount="reservation.price_amount"
+            :price_currency="reservation.price_currency"
+            :nodes="reservation.nodes"
+            :num_passengers="reservation.passengers.length"
+            :reservation_id="reservation._id"
+            @delete="reservationToDelete = reservation"/>
         </div>
-
       </div>
     </div>
-  </div>
+    <ConfirmationPopup v-if="reservationToDelete" 
+        :title="'Delete Reservation'" 
+        :question="'Are you sure you want to delete this reservation? This action cannot be undone.'" 
+        @confirm="delReservation(reservationToDelete._id)" 
+        @cancel="reservationToDelete = null"
+    />
 </template>
 
 <script setup>
-import {ref, computed, onMounted } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
-
-import { getUser } from '@/api/users.js';
 import BaseBanner from "@/components/BaseBanner.vue";
+import ReservationCard from "@/components/ReservationCard.vue";
+import { getUser } from '@/api/users.js';
+import { getUserReservations, deleteReservation } from '@/api/reservations';
+import ConfirmationPopup from '@/components/ConfirmationPopup.vue';
 
-// Store e State
 const route = useRoute();
 const user_id = computed(() => route.params.user_id);
 const user = ref(null);
 const reservations = ref([]);
-const isLoading = ref(false);
-const error = ref(null);
+const reservationToDelete = ref(null);
 
 const displaySubtitle = computed(() => {
   return user.value?.email || "";
 });
 
-/**
- * Load reservations
- */
-const loadReservations = async () => {
-  isLoading.value = true;
-  try {
-    user.value = await getUser(localStorage.getItem("authToken"), user_id.value);
-  } catch (e) {
-    console.error("Error:", e);
-    error.value = "Unable to retrieve user.";
-  } finally {
-    isLoading.value = false;
-  }
+const fetchUser = async () => {
+    try {
+        user.value = await getUser(localStorage.getItem('authToken'), user_id.value);
+    } catch (error) {
+        console.error('Error fetching user:', error);
+    }
 };
 
-onMounted(loadReservations);
+const fetchReservations = async () => {
+    try {
+        reservations.value = await getUserReservations(localStorage.getItem('authToken'), user_id.value);
+    } catch (error) {
+        console.error('Error fetching reservations:', error);
+    }
+};
+
+const delReservation = async (reservationId) => {
+    reservationToDelete.value = null;
+    try {
+        await deleteReservation(localStorage.getItem('authToken'), reservationId);
+    } catch (error) {
+        console.error('Error deleting reservation:', error);
+    }
+    reservations.value = reservations.value.filter(reservation => reservation._id !== reservationId);
+};
+
+onMounted(() => {
+    fetchUser();
+    fetchReservations();
+});
 </script>
